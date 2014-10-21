56428cbfc989d749f909f43331b1991d479b3cd5
Vit <vit@Vits-MacBook-Pro.local>
2014-10-21 12:35:30 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -9,6 +9,19 @@
 #import "MainScene.h"
 #import "Obstacle.h"
 
+@interface CGPointObject : NSObject
+{
+    CGPoint _ratio;
+    CGPoint _offset;
+    CCNode *__unsafe_unretained _child; // weak ref
+}
+@property (nonatomic,readwrite) CGPoint ratio;
+@property (nonatomic,readwrite) CGPoint offset;
+@property (nonatomic,readwrite,unsafe_unretained) CCNode *child;
++(id) pointWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
+-(id) initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
+@end
+
 @implementation MainScene {
     
     CGPoint _cloudParallaxRatio;
@@ -43,20 +56,9 @@
     
     int points;
 }
-@end
 
-@interface CGPointObject : NSObject
-{
-    CGPoint _ratio;
-    CGPoint _offset;
-    CCNode *__unsafe_unretained _child; // weak ref
-}
-@property (nonatomic,readwrite) CGPoint ratio;
-@property (nonatomic,readwrite) CGPoint offset;
-@property (nonatomic,readwrite,unsafe_unretained) CCNode *child;
-+(id) pointWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
--(id) initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
-@end
+
+
 
 - (void)didLoadFromCCB {
     self.userInteractionEnabled = TRUE;
